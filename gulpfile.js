86cab1cbac6d3c527ce425ce4e499c36e758b50e
makobasuri <makobasuri@gmail.com>
2018-04-05 22:35:52 +0200

    added basic tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,35 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const hbs = require('gulp-hb')
 const sync = require('browser-sync')
+const rename = require('gulp-rename');
 
-var styles = function styles() {
-	return gulp.src('./scss/*.scss')
+const styles = () => {
+	return gulp.src('./src/scss/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./build/css'))
+}
+
+const renderTpls = () => {
+	return gulp.src('./src/templates/*.hbs')
+		.pipe(hbs().partials('./src/templates/partials/**/*.hbs'))
+		.pipe(rename({extname: '.html'}))
+		.pipe(gulp.dest('./build'))
+}
+
+const watchStyles = () => gulp.watch('./src/scss/**/*.scss', styles)
+const watchTemplates = () => gulp.watch('./src/templates/**/*.hbs', renderTpls)
+
+const watch = gulp.series(watchStyles, watchTemplates)
+
+const build = gulp.parallel(styles, renderTpls)
+const dev = gulp.series(build, watch)
+
+module.exports = {
+	styles,
+	renderTpls,
+	watchStyles,
+	watchTemplates,
+	watch,
+	build,
+	dev
 }
