881e1d209f52d16a7f0b7a6ce8e9ec3f51b876a1
makobasuri <makobasuri@gmail.com>
2018-04-08 20:25:36 +0200

    added simple templating and babel transpiling with webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,87 @@
 const gulp = require('gulp')
-const sass = require('gulp-sass')
 const hbs = require('gulp-hb')
+const plumber = require('gulp-plumber')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
 const sync = require('browser-sync')
-const rename = require('gulp-rename');
+const webpack = require('webpack')
+const path = require('path')
+const UglifyJs = require('uglifyjs-webpack-plugin')
+
+const onError = function(error) {
+	console.log(err.toString())
+	this.emit('end')
+}
 
 const styles = () => {
 	return gulp.src('./src/scss/*.scss')
-		.pipe(sass().on('error', sass.logError))
+		.pipe(plumber(onError))
+		.pipe(sass({
+			includePaths: ['src/scss'],
+			precision: 8
+		}))
+		.pipe(postcss(autoprefixer))
 		.pipe(gulp.dest('./build/css'))
 }
 
 const renderTpls = () => {
 	return gulp.src('./src/templates/*.hbs')
+		.pipe(plumber(onError))
 		.pipe(hbs().partials('./src/templates/partials/**/*.hbs'))
 		.pipe(rename({extname: '.html'}))
 		.pipe(gulp.dest('./build'))
 }
 
+const scripts = (done) => {
+	webpack({
+		context: __dirname,
+		entry: './src/js/index.js',
+		devtool: 'cheap-module-eval-source-map',
+		output: {
+			path: path.resolve(__dirname, 'build/js'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			}]
+		},
+		plugins: [
+			new UglifyJs({
+				parallel: 4,
+				sourceMap: true
+			})
+		]
+	}).run(function(err, stats) {
+		if (err) {
+			onError(err)
+		}
+
+		done();
+	})
+}
+
+const server = (done) => {
+	sync.init({
+		server: {
+			baseDir: './build'
+		},
+		ui: false
+	})
+}
+
 const watchStyles = () => gulp.watch('./src/scss/**/*.scss', styles)
 const watchTemplates = () => gulp.watch('./src/templates/**/*.hbs', renderTpls)
+const watchScripts = () => gulp.watch('./src/js/**/*.js', scripts)
 
-const watch = gulp.series(watchStyles, watchTemplates)
+const watch = gulp.parallel(watchStyles, watchTemplates, watchScripts)
 
-const build = gulp.parallel(styles, renderTpls)
-const dev = gulp.series(build, watch)
+const build = gulp.parallel(renderTpls, styles, scripts)
+const dev = gulp.series(build, gulp.parallel(watch, server))
 
 module.exports = {
 	styles,
